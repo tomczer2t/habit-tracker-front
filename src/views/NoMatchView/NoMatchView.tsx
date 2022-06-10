d7f8933a1cec032e00d902260831e81f08af2e04
Tomasz Czerwiński <czerwitom@gmail.com>
2022-06-10 10:12:54 +0200

    Styled not foundend view page.

--- src/views/NoMatchView/NoMatchView.tsx
+++ src/views/NoMatchView/NoMatchView.tsx
@@ -1,10 +1,15 @@
 import './NoMatchView.css';
+import { Link } from 'react-router-dom';
 
 export const NoMatchView = () => {
 
   return (
     <article className="NoMatchView">
-      <h1>Page not found</h1>
+      <div className="NoMatchView__404-box">
+        <h1 className="NoMatchView__title">Page not found</h1>
+        <p className="NoMatchView__background-text">404</p>
+      </div>
+      <Link to="/" className="NoMatchView__link">Back to homepage</Link>
     </article>
   );
 };
