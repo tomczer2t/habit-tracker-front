2a6ab45f3602e50db71b7704b7b792b86586486d
Tomasz Czerwiński <czerwitom@gmail.com>
2022-06-13 12:59:50 +0200

    Added links to linkedin and to email in page footer.

--- src/components/layout/PageFooter/PageFooter.tsx
+++ src/components/layout/PageFooter/PageFooter.tsx
@@ -1,10 +1,19 @@
+import { HiOutlineMail } from 'react-icons/hi';
+import { FaLinkedinIn } from 'react-icons/fa';
+
 import './PageFooter.css';
 
 export const PageFooter = () => {
 
   return (
     <footer className="PageFooter">
-      <p>Tomasz Czerwiński</p>
+      <div className="PageFooter__content">
+        <p className="PageFooter__icons">
+          <a href="mailto:czerwitom@gmail.com" target="_blank" rel="noreferrer"><HiOutlineMail /></a>
+          <a href="https://www.linkedin.com/in/tomasz-czerwi%C5%84ski-751b6b231/" target="_blank" rel="noreferrer"><FaLinkedinIn /></a>
+        </p>
+        <p>Tomasz Czerwiński</p>
+      </div>
       <p>This page is Everyday Habit Tracker app clone. Designed only for practice and study purpose.</p>
     </footer>
   );
