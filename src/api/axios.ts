b3401306b93204bb12c56464f41c07cf5ce09de6
Tomasz Czerwiński <czerwitom@gmail.com>
2022-06-16 12:20:03 +0200

    Changed BASE_URL.

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,6 @@
 import axiosDefault from 'axios';
 
-const BASE_URL = 'http://192.168.1.27:3001/api';
+const BASE_URL = 'https://habit-tracker.networkmanager.pl/api';
 // const BASE_URL = 'http://localhost:3001/api';
 
 export const axios = axiosDefault.create({
