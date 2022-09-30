8c74f6e1f4a879dd34bb602ff9b5fc424e4be8c6
Tomasz Czerwiński <czerwitom@gmail.com>
2022-09-30 08:54:19 +0200

    Code format.

--- src/components/SpecificHabit/HabitHistory/StatsTable/StatsTable.tsx
+++ src/components/SpecificHabit/HabitHistory/StatsTable/StatsTable.tsx
@@ -12,9 +12,13 @@ export const StatsTable = ({ stats, color }: Props) => {
     <div className="StatsTable">
       { stats.map((stat, index) => {
         let textStatus: string;
-        if (stat === 0) textStatus = 'undone';
-        else if (stat === 2) textStatus = 'done';
-        else textStatus = 'skipped';
+        if (stat === 0) {
+          textStatus = 'undone';
+        } else if (stat === 2) {
+          textStatus = 'done';
+        } else {
+          textStatus = 'skipped';
+        }
 
         return <div key={ index }
                     className={ `StatsTable__cell StatsTable__cell--${ textStatus }` }
